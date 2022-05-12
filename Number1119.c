56df306965d1272929408521e45e1115b253907b
Fujimoto Kitae <ishibori81@gmail.com>
2022-05-12 11:09:53 +0900

    Test119-123

---
+++ Number1119.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int d;
+    scanf("%d", &d);
+    printf("%d", d*24);
+    return 0;
+}
---
+++ Number1123.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    float c;
+    scanf("%f", &c);
+    printf("%.3f", 9.0/5*c+32);
+    return 0;
+}
